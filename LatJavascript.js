b606eede84f71e98e36199145b8a966031c28134
FahmiGF <fahmige123@gmail.com>
2026-04-20 12:16:51 +0700

    Pertemuan 7 - Laravel

---
+++ LatJavascript.js
@@ -0,0 +1,21 @@
+/*
+ Laki-laki
+¤ Tambahkan validasi
+n Nama tidak boleh ada angka , Misal : Ade 123
+n Nama minimal 2 huruf
+n Pesan Error spesifik untuk 1 error
+
+Nama: Fahmi G
+Latihan Javascript
+*/
+
+
+
+
+alert("Anda sedang di hack oleh Seseorang! Segera inputkan nama anda dan inisial kode sebanyak 2 huruf!")
+confirm("message"); 
+prompt("message");
+
+
+
+
